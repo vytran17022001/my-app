fc3941160c1ecd883725fc348765cc71af4231ee
Anh Vy <vytran17022001@gmail.com>
2024-10-24 21:52:02 +0700

    p1- post data i uuid

--- helpers/postData.ts
+++ helpers/postData.ts
@@ -1,2 +1,11 @@
 import { db } from "@/configs/firebase";
-import { collection, addDoc } from "firebase/firestore/lite";
+import { collection, addDoc, doc } from "firebase/firestore/lite";
+import { v4 as uuidv4 } from "uuid";
+
+interface dataTypes {
+  [key: string]: any; // attribute other than any type
+}
+
+function postData(nameCollection: string, data: dataTypes) {}
+
+export default postData;
